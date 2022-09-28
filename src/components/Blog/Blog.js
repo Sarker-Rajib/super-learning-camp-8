03a67aea6db38714c030b72e357876cd6cee0b89
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-09-29 01:26:50 +0600

    blog question are added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,9 +4,18 @@ const Blog = () => {
    return (
       <div className="blog pt-4">
          <div className="container">
-            <div className="row">
-               <div className="border shadow-lg">
-                  <h1>This is Blog Heading</h1>
+            <div className="row gy-3">
+               <div className="border shadow-lg rounded">
+                  <h1>Let's dive into Blog</h1>
+               </div>
+               <div className="border shadow p-3 text-start rounded">
+                  <h5>Q. How does React Work ?</h5>
+                  <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
+                  <br />
+
+                  <h5>Q. How does React Work ?</h5>
+                  <p>The key difference between props and state is that state is internal and controlled by the component itself while props are external and controlled by whatever renders the component.</p>
+
                </div>
             </div>
          </div>
